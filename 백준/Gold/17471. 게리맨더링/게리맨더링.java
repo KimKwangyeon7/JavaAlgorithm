9bacf30076e55e92e81328fa2d54cd8edca742f4
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2023-09-06 03:07:23 +0900

    [Gold IV] Title: 게리맨더링, Time: 88 ms, Memory: 12680 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,116 +1,111 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
- * @author 김광연 
+ * @author kwang
  *
  */
 public class Main {
-	
-	static List<Integer>[] list;
-	static int[] res, popul, tmp, r;
-	static int N, total, ans;
+	static int N, ans;
+	static int[][] board;
+	static int[] popul;
+	static boolean[] res, another;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
 		N = Integer.parseInt(br.readLine());
-		popul = new int[N]; // 각 구역 인구 수 담을 배열
+		board = new int[N][N]; // 인접 구역 정보 담을 배열
+		popul = new int[N]; // 인구수 담을 배열
 		
-		total = 0;
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			popul[i] = Integer.parseInt(st.nextToken());
-			total += popul[i];
 		}
-		list = new ArrayList[N];
-		for (int i = 0; i < N; i++) {
+		
+		int cnt = 0;
+		for (int i = 0; i < N; i++) { 
 			st = new StringTokenizer(br.readLine());
-			int cnt = Integer.parseInt(st.nextToken());
-			list[i] = new ArrayList<>();
-			for (int j = 0; j < cnt; j++) { // 간선 정보 입력 받기
-				int tmp = Integer.parseInt(st.nextToken()) - 1; // 인덱스를 0부터 시작했기 때문에 1 빼고 넣어주기
-				list[i].add(tmp);
+			cnt = Integer.parseInt(st.nextToken()); // 해당 도시의 인접 구역 개수
+			for (int j = 0; j < cnt; j++) {
+				board[i][Integer.parseInt(st.nextToken())-1] = 1;
 			}
 		}
-		r = new int[N];
-		ans = 2147000000;
+		ans = Integer.MAX_VALUE;
 		for (int i = 1; i <= N/2; i++) {
-			part(i,0, 0);
+			res = new boolean[N]; // 고른 인덱스 체크할 불리언 배열	
+			combi(i, 0, 0); // 고를 개수, 현재 고른 개수, 시작 인덱스
 		}
-		if (ans == 2147000000) {
+		if (ans == Integer.MAX_VALUE) {
 			System.out.println(-1);
 		} else {
 			System.out.println(ans);
 		}
 	}
-	
-	static void part(int tsize, int size, int start) {
-		if (size == tsize) {
-			res = new int[size];
-			for (int i = 0; i < size; i++) {
-				res[i] = r[i];
+	private static void combi(int size, int cnt, int start) {
+		if (cnt == size) {
+			another = new boolean[N];
+			for (int i = 0; i < N; i++) {
+				if (!res[i]) { // 고르지 않았다면
+					another[i] = true; // 상대 체크 배열을 true로
+				}
 			}
-			tmp = new int[N-size];
+			if (isGary(res) && isGary(another)) { // 둘다 선거구 가능하면
+				int sum1 = 0;
+				int sum2 = 0;
+				for (int i = 0; i < N; i++) {
+					if (res[i]) {
+						sum1 += popul[i];
+					}
+					if (another[i]) {
+						sum2 += popul[i];
+					}
+				}
+				ans = Math.min(ans, Math.abs(sum1-sum2));
+			}
 			
-			int flag, idx = 0;
-			for (int i = 0; i < N; i++) { // res의 차집합 구하기
-				flag = 0;
-				for (int j = 0; j < size; j++) {
-					if (res[j] == i) {
-						flag = 1;
-						break;
-					}		
-				}
-				if (flag == 0) {
-					tmp[idx++] = i;
-				}
-			}
-
-			if (isRoute(res) && isRoute(tmp)) {
-				// 인구수 차이 구하기
-				int sum = 0; 
-				for (int k = 0; k < size; k++) {
-					sum += popul[res[k]];
-				}
-				ans = Math.min(ans, Math.abs(total-sum-sum));
-			}
 			return;
 		}
 		for (int i = start; i < N; i++) {
-			r[size] = i;
-			part(tsize, size+1, i+1); // 골랐을 때
+			if (!res[i]) {
+				res[i] = true;
+				combi(size, cnt+1, i+1);
+				res[i] = false;
+			}
 		}
 	}
-	static boolean isRoute(int[] res) {
-		boolean[] check = new boolean[N];
-		for (int i = 0; i < res.length; i++) {
-			check[res[i]] = true;
+	private static boolean isGary(boolean[] res) { // 해당 선거구가 서로 다 통하는지 확인하는 메서드
+		int idx = 0;
+		for (int i = 0; i < N; i++) {
+			if (res[i]) { // 해당 인덱스가 선거구에 들면 
+				idx = i; // 그 인덱스를 저장 -> 시작 인덱스로 하기 위해
+				break;
+			}
 		}
+		// BFS
 		Queue<Integer> qu = new ArrayDeque<>();
 		boolean[] visit = new boolean[N];
-		qu.offer(res[0]);
-		visit[res[0]] = true;
-		
+		qu.offer(idx);
+		visit[idx] = true;
+
 		while (!qu.isEmpty()) {
-			int t = qu.poll();
-			for (int n: list[t]) {
-				if (check[n] && !visit[n]) { // 인접 정점이면서 방문 X
-					visit[n] = true;
-					qu.offer(n);
+			int tmp = qu.poll();
+			for (int i = 0; i < N ; i++) {
+				if (board[tmp][i] == 1 && res[i] && !visit[i]) { // 선거구에 속하며 인접노드이고 방문하지 않았다면
+					visit[i] = true; // 방문 체크
+					qu.offer(i); // 큐에 추가
 				}
 			}
 		}
-		for (int i = 0; i < res.length; i++) {
-			if (!visit[res[i]]) { // res에 속한 모든 정점을 방문하지 못했으면 false 반환
-				return false;
+		// 선택한 애들을 다 순회 가능한 지 확인
+		for (int i = 0; i < N; i++) {
+			if (res[i] && !visit[i]) { 
+				return false; 
 			}
 		}
-		return true; // 모두 방문했으면 true 반환
+		return true;
 	}
+
 }
