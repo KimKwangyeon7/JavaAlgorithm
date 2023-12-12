459325b20dd79b1f2c8832f7480a94c9b1ed2118
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2023-12-12 19:01:16 +0900

    [Gold IV] Title: 비슷한 단어, Time: 212 ms, Memory: 17060 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -1,6 +1,8 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author 김광연
@@ -10,21 +12,27 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine()); // 문자열의 개수
-		String[] words = new String[N]; // 문자열들을 담을 배열
-		
+		List<String> str = new ArrayList<>();
 		for (int i = 0; i < N; i++) { // 입력 저장
-			words[i] = br.readLine();
+			str.add(br.readLine());
 		}
 		
 		int max = -1;
 		String S = "";
 		String T = "";
+				
 		for (int i = 0; i < N; i++) {
+			if (str.get(i).length() < max) {
+				continue;
+			}
 			for (int j = i+1; j < N; j++) {
-				if (prefix(words[i], words[j]) > max) { // 두문자열의 공통 접두사 길이가 더 크면
-					max = prefix(words[i], words[j]);
-					S = words[i];
-					T = words[j];
+				if (str.get(j).length() < max) {
+					continue;
+				}
+				if (prefix(str.get(i), str.get(j)) > max) { // 두문자열의 공통 접두사 길이가 더 크면
+					max = prefix(str.get(i), str.get(j)); // max, S, T 값 update
+					S = str.get(i); 
+					T = str.get(j);
 				}
 			}
 		}
@@ -37,13 +45,13 @@ public class Main {
 	static int prefix(String a, String b) { // 두 문자열의 공통 접두사 길이 구하는 메서드
 		int lenA = a.length(); // 문자열 a의 길이
 		int lenB = b.length(); // 문자열 b의 길이
-		int cnt = 0;
+		int cnt = 0; // 공통 접두사
 		
-		for (int i = 0; i < Math.min(lenA, lenB); i++) {
-			if (a.charAt(i) != b.charAt(i)) {
+		for (int i = 0; i < Math.min(lenA, lenB); i++) { // 두 문자열 중 더 길이가 짧은 길이까지 비교
+			if (a.charAt(i) != b.charAt(i)) { // 해당 위치의 문자가 다르면 탈출
 				break;
 			}
-			cnt++;
+			cnt++; // 같으면 cnt 1 증가
 		}
 		return cnt;
 	}
