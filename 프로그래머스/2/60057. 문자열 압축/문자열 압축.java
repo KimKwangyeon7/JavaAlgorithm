5df0de612f35f3b409ea28062d402467c243c8a1
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2024-07-21 19:57:42 +0900

    [level 2] Title: 문자열 압축, Time: 3.22 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -0,0 +1,82 @@
+import java.util.HashSet;
+class Solution {
+    static int min;
+    public int solution(String s) {
+        int answer = 0;
+        int len = s.length();
+        
+        min = len;
+        if (len <= 2){
+            return len;
+        } else if (len == 3){
+            if (s.charAt(0) == s.charAt(1) && s.charAt(1) == s.charAt(2)){
+                return 2;
+            } else {
+                return 3;
+            }
+        } else {
+            for (int i = 1; i <= len/2; i++){
+                min = Math.min(min, zip(i, s));
+                // int a = zip(i, s);
+                // if (min > a){
+                //     System.out.println(i);
+                //     min = a;
+                // }
+            }
+        }
+        
+        return min;
+    }
+    static int zip(int size, String s){
+        HashSet<String> set = new HashSet<>();
+        int cnt = 0;
+        int idx = 0;
+        int len = s.length();
+        int flag = 1;
+        while (true){
+            if (min <= cnt){
+                break;
+            }
+            if (idx+size <= len){
+                String tmp = s.substring(idx, idx+size);
+                if (set.contains(tmp)){
+                    flag++;
+                } else {
+                    if (flag == 1){
+                        flag = 1;
+                    }
+                    else if (flag < 10){
+                        cnt++;
+                    } else if (flag < 100){
+                        cnt += 2;
+                    } else if (flag < 1000) {
+                        cnt += 3;
+                    } else {
+                        cnt += 4;
+                    }
+                    flag = 1;
+                    set = new HashSet<>();
+                    set.add(tmp);
+                    cnt += size;      
+                }
+            } else {
+                cnt += len - idx;
+                break;
+            }
+            idx = idx + size;
+        }
+        if (flag == 1){
+            flag = 1;
+        }
+        else if (flag < 10){
+            cnt++;
+        } else if (flag < 100){
+            cnt += 2;
+        } else if (flag < 1000) {
+            cnt += 3;
+        } else {
+            cnt += 4;
+        }
+        return cnt;
+    }
+}
