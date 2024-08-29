a9fdcc19eb05bd7c23beb8f09f636fb88978c997
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2024-08-29 19:13:13 +0900

    [level 2] Title: 배달, Time: 4.88 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    static class Dot implements Comparable<Dot>{
+        int to;
+        int time;
+        public Dot(int to, int time){
+            this.to = to;
+            this.time = time;
+        }
+        public int compareTo(Dot o){
+            return Integer.compare(this.time, o.time);
+        }
+    }
+    public int solution(int N, int[][] road, int K) {
+        int answer = 0;
+        PriorityQueue<Dot> pq = new PriorityQueue<>();
+        List<Dot>[] towns = new ArrayList[N+1];
+        for (int i = 0; i < N+1; i++){
+            towns[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < road.length; i++){
+            int[] tmp = road[i];
+            towns[tmp[0]].add(new Dot(tmp[1], tmp[2]));
+            towns[tmp[1]].add(new Dot(tmp[0], tmp[2]));
+        }
+        int[] dist = new int[N+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        pq.offer(new Dot(1, 0));
+        dist[1] = 0;
+        while (!pq.isEmpty()){
+            Dot tmp = pq.poll();
+            
+            for (int i = 0; i < towns[tmp.to].size(); i++){
+                Dot next = towns[tmp.to].get(i);
+                if (dist[next.to] > dist[tmp.to] + next.time){
+                    dist[next.to] = dist[tmp.to] + next.time;
+                    pq.offer(new Dot(next.to, dist[next.to]));
+                }
+            }
+        }
+        for (int i = 1; i < N+1; i++){
+            if (dist[i] <= K){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
