2176c80f57428d85ec2558564b7d2f3c66ca317b
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2024-10-03 17:13:52 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 279.46 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class Solution {
+    static class Dot implements Comparable<Dot>{
+        int idx;
+        int time;
+        public Dot(int idx, int time){
+            this.idx = idx;
+            this.time = time;
+        }
+        public int compareTo(Dot o){
+            if (time == o.time){
+                return Integer.compare(idx, o.idx);
+            }
+            return Integer.compare(time, o.time);
+        }
+    }
+    public int solution(int[] food_times, long k) {
+        int answer = 0;
+        long len = food_times.length;
+        List<Dot> list = new ArrayList<>();
+        PriorityQueue<Dot> pq = new PriorityQueue<>();
+        long sum = 0;
+        for (int i = 0; i < len; i++){
+            pq.offer(new Dot(i, food_times[i]));
+            sum += food_times[i];
+        }
+        if (sum <= k){
+            return -1;
+        }
+        long cnt = 0;
+        long total = len;
+        long prev = 0;
+        long r = -1;
+        while (cnt + (pq.peek().time - prev) * total <= k){
+            Dot tmp = pq.poll();
+            if (tmp.time == prev){
+                total--;
+                continue;
+            }
+            cnt += (tmp.time-prev) * total;
+            prev = tmp.time;
+            total--;
+        }
+        
+        while (!pq.isEmpty()){
+            list.add(pq.poll());
+        }
+        Collections.sort(list, new Comparator<Dot>() {
+            @Override
+            public int compare(Dot a, Dot b) {
+                return Integer.compare(a.idx, b.idx);
+            }
+        });
+        //Collections.sort(list);
+        answer = list.get((int)((k-cnt)%total)).idx+1;
+
+        return answer;  
+        
+    }
+}
