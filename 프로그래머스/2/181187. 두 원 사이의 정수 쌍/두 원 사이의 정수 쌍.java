23e0d75b8d57e767cb983ecaa59c6e065f893cf3
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2024-11-18 14:51:48 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 29.35 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long solution(int r1, int r2) {
+        long answer = 0;
+        for (int i = r2-1; i > 0; i--){
+            long high = (long)Math.sqrt(Math.pow(r2, 2) - Math.pow(i, 2));
+            if (Math.pow(i, 2) > Math.pow(r2, 2)){
+                answer += high;
+            } else {
+                double tmp = Math.sqrt(Math.pow(r1, 2) - Math.pow(i, 2));
+                long low = (long)tmp;
+                if (tmp - (double)low == 0){
+                    answer += high - low + 1;
+                } else {
+                    answer += high - low;
+                }  
+            }    
+        }
+        answer -= 1;
+        answer *= 4;
+        answer += (r2-r1+1)*4;
+        return answer;
+    }
+}
