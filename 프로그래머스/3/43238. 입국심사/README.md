db0ea3474b1742975f476cad8afce5df637051f9
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2024-12-03 17:44:45 +0900

    [level 3] Title: 입국심사, Time: 73.99 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 91.4 MB, 시간: 73.71 ms
+메모리: 93.8 MB, 시간: 73.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 16일 13:25:08
+2024년 12월 03일 17:44:42
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,51 +1,26 @@
 import java.util.*;
 class Solution {
-    static class Dot implements Comparable<Dot>{
-        long cnt;
-        long start;
-        
-        public Dot(long cnt, long start){
-            this.cnt = cnt;
-            this.start = start;
-        }
-        public int compareTo(Dot o){
-            return Long.compare(this.cnt*this.start, o.cnt*o.start);
-        }
-    }
     public long solution(int n, int[] times) {
-        long answer = 0;
+        long answer = -1;
         long left = 0;
         long right = (long)Math.pow(10, 18);
         long mid = (left + right) / 2;
+        
         while (left <= right){
             mid = (left + right) / 2;
-            if (check(mid, times) < n){
+            
+            if (test(mid, times) < n){
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        
-        //System.out.println(left + " " + right);
         return left;
-        
-        
-        
-//         PriorityQueue<Dot> pq = new PriorityQueue<>();
-//         for (int t: times){
-//             pq.offer(new Dot(1, (long)t));
-//         }
-        
-//         for (int i = 0; i < n; i++){
-//             Dot tmp = pq.poll();
-//             pq.offer(new Dot(tmp.cnt+1, tmp.start));
-//             answer = tmp.cnt * tmp.start;
-//         }
-//        return answer;
     }
-    private static long check(long time, int[] times){
+    private static long test(long time, int[] times){
         int len = times.length;
         long total = 0;
+        
         for (int i = 0; i < len; i++){
             total += time / (long)times[i];
         }
