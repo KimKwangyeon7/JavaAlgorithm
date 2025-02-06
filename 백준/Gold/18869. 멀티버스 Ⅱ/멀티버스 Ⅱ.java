244475c049f7dcfd4876aa36f875dd804a9dbb87
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2025-02-06 12:19:21 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 632 ms, Memory: 137720 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
@@ -1,79 +1,66 @@
-
-
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-//	static class Planet implements Comparable<Planet>{
-//		int size;
-//		int idx;
-//		
-//		public Planet(int size, int idx) {
-//			this.size = size;
-//			this.idx = idx;
-//		}
-//		public int compareTo(Planet o) {
-//			if (this.size == o.size) {
-//				o.idx = this.idx;
-//				return Integer.compare(this.idx, o.idx);
-//			}
-//			return Integer.compare(this.size, o.size);
-//		}
-//	}
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
 	
-		List<int[]> universeList = new ArrayList<>();
-		for (int i = 0; i < M; i++) {
-			int[] universe = new int[N];
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				int planet = Integer.parseInt(st.nextToken());
-				universe[j] = planet;
-			}
-			int[] tmp = universe.clone();
-			Arrays.sort(tmp);
-			int[] sortedPlanet = new int[N];
-			for (int k = 0; k < N; k++) {
-				int rank = binarySearch(universe[k], tmp);
-				sortedPlanet[k] = rank;
-			}
-			universeList.add(sortedPlanet);
+	static class Planet implements Comparable<Planet>{
+		int value;
+		int idx;
+		public Planet(int value,int idx) {
+			this.value = value;
+			this.idx = idx;
 		}
 		
+		public int compareTo(Planet o) {
+			return Integer.compare(this.value, o.value);
+		}
+		public String StringTo() {
+			return this.value+" "+this.idx;
+		}
+	}
+	
+	static int[][] indexs;
+	static int M;
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		indexs = new int[N][M];
+		
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			int[] space = new int[M];
+			for(int j=0;j<M;j++) {
+				int n = Integer.parseInt(st.nextToken());
+				space[j] = n;
+			}
+			
+			for(int j=0;j<M;j++) {
+				indexs[i][j] = Arrays.binarySearch(space, space[j]);
+			}
+
+		}
 		int ans = 0;
-		for (int i = 0; i < M-1; i++) {
-			for (int j = i+1; j < M; j++) {
-				int flag = 1;
-				for (int k = 0; k < N; k++) {
-					if (universeList.get(i)[k] != universeList.get(j)[k]) {
-						flag = 0;
+		for(int i=0;i<N-1;i++) {
+			for(int j=i+1;j<N;j++) {
+				boolean flag = true;
+				for(int k=0;k<M;k++) {
+					if(indexs[i][k] != indexs[j][k]) {
+						flag = false;
 						break;
 					}
 				}
-				if (flag == 1) {
-					ans++;
-				}
+				if(flag)ans++;
 			}
 		}
 		System.out.println(ans);
-	}
-	private static int binarySearch(int target, int[] tmp) {
-		int left = 0;
-		int right = tmp.length-1;
 		
-		while (left <= right) {
-			int mid = (left+right) / 2;
-			if (tmp[mid] < target) {
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
-		return left;
 	}
+	
+
+
+
+
 }
