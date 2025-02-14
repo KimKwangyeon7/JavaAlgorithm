0d4921324f880c61f570a049e5f61ade2cb4bfc9
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2025-02-14 13:26:39 +0900

    [Gold IV] Title: 전화번호 목록, Time: 368 ms, Memory: 192032 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -3,54 +3,69 @@
 
 import java.io.*;
 import java.util.*;
-/**
- * @author kwang
- *
- */
+
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
-			int n = Integer.parseInt(br.readLine());
-			Set<String> set = new HashSet<>();
-			List<String> list = new ArrayList<>();
-			for (int i = 0; i < n; i++) {
-				String tmp = br.readLine();
-				set.add(tmp);
-				list.add(tmp);
-			}
-			int flag = 0;
-			for (String str: list) {
-				String tmp = "";
-				for (char c: str.toCharArray()) {
-					tmp += c;
-					//System.out.println(tmp);
-					if (set.contains(tmp)) {
-						if (tmp.length() == str.length()) {
-							continue;
-						}
-						flag = 1;
-						break;
-					}
-				}
-				if (flag == 1) {
-					break;
-				}
-			}
-			if (flag == 1) {
-				sb.append("NO").append("\n");
-			} else {
-				sb.append("YES").append("\n");
-			}
-		}
-		System.out.println(sb);
-	}
+    static class TrieNode {
+        Map<Character, TrieNode> child = new HashMap<>();
+        boolean isLast;
+    }
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            int n = Integer.parseInt(br.readLine());
+            TrieNode root = new TrieNode();
+            boolean isConsistent = true;  // 일관성 여부
+            int flag = 0;
+            //String[] numbers = new String[n];
+            for (int i = 0; i < n; i++) {
+                String number = br.readLine();
+                if (!isConsistent) {
+                	continue;
+                }
+                if (!insert(number, root)) {  // insert 과정에서 접두어 문제 발생 시 false 반환
+                    isConsistent = false;
+                }
+            }
+
+            // 입력 정렬 (사전순 정렬하면 접두어 여부를 더 빠르게 확인 가능)
+//            Arrays.sort(numbers);
+//
+//            for (String number : numbers) {
+//                if (!insert(number, root)) {  // insert 과정에서 접두어 문제 발생 시 false 반환
+//                    isConsistent = false;
+//                    break;
+//                }
+//            }
+
+            sb.append(isConsistent ? "YES" : "NO").append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static boolean insert(String tmp, TrieNode root) {
+        TrieNode cur = root;
+        for (char c : tmp.toCharArray()) {
+            if (!cur.child.containsKey(c)) {
+                cur.child.put(c, new TrieNode());
+            }
+            cur = cur.child.get(c);
+
+            if (cur.isLast) {
+                return false; // 현재 경로가 이미 등록된 번호이면 일관성 깨짐
+            }
+        }
+
+        // 현재 번호를 추가할 때, 이미 하위 노드가 있다면 접두어 문제 발생
+        if (!cur.child.isEmpty()) {
+            return false;
+        }
+
+        cur.isLast = true;
+        return true;
+    }
 }
