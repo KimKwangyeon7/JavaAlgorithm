962ed7576e37b6c116d63913bc296d880496015a
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2025-02-28 19:37:44 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
 
 ### 성능 요약
 
-메모리: 80.5 MB, 시간: 0.06 ms
+메모리: 76.8 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 15일 11:54:05
+2025년 02월 28일 19:37:41
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,32 +1,62 @@
-import java.util.ArrayList;
+import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] array = new int[progresses.length];
-        for (int i = 0; i < progresses.length; i++) {
-            int day = 0;
-            while (progresses[i] < 100) {
-                progresses[i] += speeds[i];
-                day++;
+        int N = progresses.length;
+        int[] board = new int[N];
+        for (int i = 0; i < N; i++){
+            board[i] = (100-progresses[i]) / speeds[i];
+            if((100-progresses[i]) % speeds[i] > 0){
+                board[i]++;
             }
-            array[i] = day;
         }
-        ArrayList<Integer> list = new ArrayList<>();
-        int cur = array[0];
-        int count = 1;
-        for (int i = 1; i < array.length; i++) {
-            if (cur >= array[i]) {
-                count++;
+        
+        List<Integer> list = new ArrayList<>();
+        int cnt = 1;
+        int max = board[0];
+        for (int i = 1; i < N; i++){
+            if (board[i] <= max){
+                cnt++;
             } else {
-                list.add(count);
-                cur = array[i];
-                count = 1;
+                list.add(cnt);
+                max = board[i];
+                cnt = 1;
             }
         }
-        list.add(count);
+        list.add(cnt);
         int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++){
             answer[i] = list.get(i);
         }
         return answer;
+        
+        
+        
+        // int[] array = new int[progresses.length];
+        // for (int i = 0; i < progresses.length; i++) {
+        //     int day = 0;
+        //     while (progresses[i] < 100) {
+        //         progresses[i] += speeds[i];
+        //         day++;
+        //     }
+        //     array[i] = day;
+        // }
+        // ArrayList<Integer> list = new ArrayList<>();
+        // int cur = array[0];
+        // int count = 1;
+        // for (int i = 1; i < array.length; i++) {
+        //     if (cur >= array[i]) {
+        //         count++;
+        //     } else {
+        //         list.add(count);
+        //         cur = array[i];
+        //         count = 1;
+        //     }
+        // }
+        // list.add(count);
+        // int[] answer = new int[list.size()];
+        // for (int i = 0; i < list.size(); i++) {
+        //     answer[i] = list.get(i);
+        // }
+        // return answer;
     }
 }
