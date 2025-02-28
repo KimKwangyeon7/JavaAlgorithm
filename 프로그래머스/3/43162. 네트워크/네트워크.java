825977712b94f8c01fa241c96a09bae5da43a2d2
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2025-02-28 16:45:51 +0900

    [level 3] Title: 네트워크, Time: 0.69 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,24 +1,61 @@
+import java.util.*;
 class Solution {
     static int[] parent;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        parent = new int[n+1];
-        for (int i = 1; i < n+1; i++){
-            parent[i] = i;
-        }
+        // parent = new int[n+1];
+        // for (int i = 1; i < n+1; i++){
+        //     parent[i] = i;
+        // }
+        // for (int i = 0; i < n; i++){
+        //     for (int j = 0; j < n; j++){
+        //         if (computers[i][j] == 1){
+        //             union(Math.min(i+1, j+1), Math.max(i+1, j+1));
+        //         }
+        //     }
+        // }
+        // for (int i = 1; i < n+1; i++){
+        //     if (parent[i] == i){
+        //         answer++;
+        //     }
+        // }
+        // return answer;
+        List<Integer>[] list = new ArrayList[n];
         for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                if (computers[i][j] == 1){
-                    union(Math.min(i+1, j+1), Math.max(i+1, j+1));
+            list[i] = new ArrayList<>();
+        }
+        
+        for (int i = 0; i < n; i++){
+            for (int j = i; j < n; j++){
+                if (i != j){
+                    if (computers[i][j] == 1){
+                        list[i].add(j);
+                        list[j].add(i);
+                    }
                 }
             }
         }
-        for (int i = 1; i < n+1; i++){
-            if (parent[i] == i){
-                answer++;
+        boolean[] visited = new boolean[n];
+        int cnt = 0;
+        for (int i = 0; i < n; i++){
+            if (!visited[i]){
+                cnt++;
+                visited[i] = true;
+                Queue<Integer> qu = new ArrayDeque<>();
+                qu.offer(i);
+                while (!qu.isEmpty()){
+                    int tmp = qu.poll();
+                    for (int next: list[tmp]){
+                        if (!visited[next]){
+                            visited[next] = true;
+                            qu.offer(next);
+                        }
+                    }
+                }
             }
+            
         }
-        return answer;
+        return cnt;
     }
     private static int find(int x){
         if (parent[x] == x){
