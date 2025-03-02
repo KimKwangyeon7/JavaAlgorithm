885ae43ba310ac4936d898939fcc3be1d4cf233c
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2025-03-02 20:42:36 +0900

    [Gold IV] Title: 팰린드롬?, Time: 672 ms, Memory: 230880 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,66 +1,56 @@
-
 import java.io.*;
 import java.util.*;
-/**
- * @author kwang
- *
- */
-public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
-		StringBuilder sb = new StringBuilder();
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+
+        // 1. 입력 받기
         int N = Integer.parseInt(br.readLine());
         int[] board = new int[N+1];
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-        	board[i] = Integer.parseInt(st.nextToken());
+            board[i] = Integer.parseInt(st.nextToken());
         }
-        int[][] dp = new int[N+1][N+1];
+
+        // 2. DP 배열 초기화
+        boolean[][] dp = new boolean[N+1][N+1];
+
+        // 3. 길이 1인 구간은 항상 팰린드롬
         for (int i = 1; i <= N; i++) {
-        	dp[i][i] = 2; 
+            dp[i][i] = true;
         }
+
+        // 4. 길이 2인 구간 (연속된 두 수가 같으면 팰린드롬)
+        for (int i = 1; i < N; i++) {
+            if (board[i] == board[i+1]) {
+                dp[i][i+1] = true;
+            }
+        }
+
+        // 5. 길이 3 이상인 구간의 팰린드롬 여부 판단
+        for (int length = 3; length <= N; length++) {  // 길이 3부터 N까지
+            for (int i = 1; i + length - 1 <= N; i++) {
+                int j = i + length - 1;  // 끝점
+                if (board[i] == board[j] && dp[i+1][j-1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        // 6. 질의 처리 (O(1)로 바로 결과 반환 가능)
         int M = Integer.parseInt(br.readLine());
         for (int i = 0; i < M; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	int s = Integer.parseInt(st.nextToken());
-        	int e = Integer.parseInt(st.nextToken());
-        	
-        	int x = s;
-        	int y = e;
-        	while (x > 0 && x <= N && y > 0 && y <= N && x <= y) {
-        		if (dp[x][y] == 0) {
-        			if (board[x] == board[y]) {
-        				dp[x][y] = 2;
-        				x++;
-        				y--;
-        				if (x > y) {
-        					sb.append(1).append("\n");
-        				}
-        			} else {
-        				for (int p = 0; x-p >= 1 && y+p <= N; p++) {
-        					dp[x-p][y+p] = 1;
-        				}
-        				sb.append(0).append("\n");
-        				break;
-        			}
-        		} else {
-        			sb.append(dp[s][e]-1).append("\n");
-        			break;
-        		}
-        	}
-        }
-//        for (int i = 1; i <= N; i++) {
-//        	for (int j = 1; j <= N; j++) {
-//        		System.out.print(dp[i][j] + " ");
-//        	}
-//        	System.out.println();
-//        }
-        System.out.print(sb.toString().trim());
-	}
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
 
+            sb.append(dp[S][E] ? "1\n" : "0\n");  // O(1)로 답변 가능
+        }
+
+        // 7. 출력
+        System.out.print(sb);
+    }
 }
