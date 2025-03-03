7462b5271e0216f95120120ce574d9356ecc2480
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2025-03-03 15:45:51 +0900

    [level 2] Title: N-Queen, Time: 99.46 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,86 +1,36 @@
 class Solution {
+    static boolean[] rightCross;
+    static boolean[] leftCross;
+    static boolean[] columns;
     static int answer;
-    //static int[] res;
     public int solution(int n) {
         answer = 0;
-        boolean[][] visited = new boolean[n][n];
-        //res = new int[n];
-        dfs(0, n, visited);
+        columns = new boolean[n];
+        leftCross = new boolean[2*n];
+        rightCross = new boolean[2*n];
+        
+        dfs(0, n);
+        
         return answer;
     }
-    private static void dfs(int L, int N, boolean[][] visited){
-        if (L == N){
+    private static void dfs(int row, int n){
+        if(row == n){
             answer++;
-            // for (int i = 0; i < N; i++){
-            //     System.out.print(res[i] + " ");
-            // }
-            // System.out.println();
             return;
         }
-        boolean[][] tmp = copyArray(visited, N);
-        for (int i = 0; i < N; i++){
-            if (!visited[L][i]){
-                visited = checkCross(visited, L, i);
-                visited = checkStraight(visited, L, i);
-                //res[L] = i;
-                dfs(L+1, N, visited);
-                for (int j = 0; j < N; j++){
-                    visited[j] = tmp[j].clone();
-                }
-            }
-        }
-    }
-    private static boolean[][] copyArray(boolean[][] visited, int N){
-        boolean[][] tmp = new boolean[N][N];
-        for (int i = 0; i < N; i++){
-            tmp[i] = visited[i].clone();
-        }
-        return tmp;
-    }
-    private static boolean[][] checkCross(boolean[][] visited, int x, int y){
-        int row = visited.length;
-        int col = visited[0].length;
-        int tmpX = x;
-        int tmpY = y;
-        while (x < row && y < col){
-            visited[x][y] = true;
-            x++;
-            y++;
-        }
-        x = tmpX;
-        y = tmpY;
-        while (x >= 0 && y < col){
-            visited[x][y] = true;
-            x--;
-            y++;
-        }
-        x = tmpX;
-        y = tmpY;
-        while (x < row && y >= 0){
-            visited[x][y] = true;
-            x++;
-            y--;
-        }
-        x = tmpX;
-        y = tmpY;
-        while (x >= 0 && y >= 0){
-            visited[x][y] = true;
-            x--;
-            y--;
-        }
-        return visited;
-    }
-    private static boolean[][] checkStraight(boolean[][] visited, int x, int y){
-        int row = visited.length;
-        int col = visited[0].length;
         
-        for (int i = 0; i < row; i++){
-            for (int j = 0; j < col; j++){
-                if (i == x || j == y){
-                    visited[i][j] = true;
-                }
+        for (int i = 0; i < n; i++){
+            if (columns[i] || leftCross[row-i+n] || rightCross[row+i]){
+                continue;
             }
+            columns[i] = true;
+            leftCross[row-i+n] = true;
+            rightCross[row+i] = true; 
+            dfs(row+1, n);
+            columns[i] = false;
+            leftCross[row-i+n] = false;
+            rightCross[row+i] = false; 
         }
-        return visited;
+        
     }
 }
--- 프로그래머스/2/12952. N－Queen/README.md
+++ 프로그래머스/2/12952. N－Queen/README.md
@@ -1,10 +1,10 @@
 # [level 2] N-Queen - 12952 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952?language=java) 
 
 ### 성능 요약
 
-메모리: 384 MB, 시간: 1080.15 ms
+메모리: 75.9 MB, 시간: 99.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 17일 19:05:54
+2025년 03월 03일 15:45:49
 
 ### 문제 설명
 
