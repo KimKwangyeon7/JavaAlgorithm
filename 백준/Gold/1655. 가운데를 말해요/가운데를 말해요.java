47b8baf73e9be87611e6f6d931c6364f0c7dc837
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2025-03-17 12:10:41 +0900

    [Gold II] Title: 가운데를 말해요, Time: 380 ms, Memory: 38924 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,38 +1,39 @@
+
 import java.io.*;
 import java.util.*;
-
+/**
+ * @author kwang
+ *
+ */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
 
-        // 최대 힙 (중간값 이하의 값들을 저장)
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        // 최소 힙 (중간값 이상의 값들을 저장)
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		Queue<Integer> quMax = new PriorityQueue<>(Collections.reverseOrder());
+		Queue<Integer> quMin = new PriorityQueue<>();
+		
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (quMax.size() == quMin.size()) {
+				quMax.offer(num);
+			} else {
+				quMin.offer(num);
+			}
+			
+			if (!quMax.isEmpty() && !quMin.isEmpty() && quMax.peek() > quMin.peek()) {
+				int n1 = quMax.poll();
+				int n2 = quMin.poll();
+				quMax.offer(n2);
+				quMin.offer(n1);
+			}
+			sb.append(quMax.peek()).append("\n");
+		}
+		System.out.print(sb.toString().trim());
+	}
 
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-
-            // 1. 항상 maxHeap에 먼저 삽입
-            if (maxHeap.size() == minHeap.size()) {
-                maxHeap.add(num);
-            } else {
-                minHeap.add(num);
-            }
-
-            // 2. maxHeap의 최대값이 minHeap의 최소값보다 크다면 swap
-            if (!maxHeap.isEmpty() && !minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) {
-                int tempMax = maxHeap.poll();
-                int tempMin = minHeap.poll();
-                maxHeap.add(tempMin);
-                minHeap.add(tempMax);
-            }
-
-            // 3. 중간값 출력 (maxHeap의 루트)
-            sb.append(maxHeap.peek()).append("\n");
-        }
-        System.out.print(sb);
-    }
 }
