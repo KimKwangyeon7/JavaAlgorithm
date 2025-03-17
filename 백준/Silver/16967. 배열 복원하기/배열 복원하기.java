09523529b0b5677dd11af63af87e89b8d4c8ed94
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2025-03-17 12:41:50 +0900

    [Silver III] Title: 배열 복원하기, Time: 332 ms, Memory: 42852 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
@@ -0,0 +1,54 @@
+
+
+/**
+ * @author kwang
+ *
+ */
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		int Y = Integer.parseInt(st.nextToken());
+		
+		int[][] board = new int[H+X][W+Y];
+		int[][] ans = new int[H][W];
+		for (int i = 0; i < H+X; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < W+Y; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
+				if ((i >= X && i <= H-1) && (j >= Y && j <= W-1)) {
+					ans[i][j] = board[i][j] - ans[i-X][j-Y];
+					sb.append(ans[i][j]);
+				} else {
+					ans[i][j] = board[i][j];
+					sb.append(board[i][j]);
+				}
+				if (j < W-1) {
+					sb.append(" ");
+				}
+			}
+			if (i < H-1) {
+				sb.append("\n");
+			}
+		} 
+		System.out.println(sb.toString().trim());
+	}
+
+}
