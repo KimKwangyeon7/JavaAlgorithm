b0ff144e3f9163c91dfdca857204b6c8660b799d
KimKwangYeon7 <114486414+KimKwangyeon7@users.noreply.github.com>
2025-04-04 15:05:25 +0900

    [Gold II] Title: 트리의 지름, Time: 684 ms, Memory: 99068 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,66 +1,74 @@
 import java.util.*;
- 
-public class Main {    
- 
-    static ArrayList<Node>[] list;
+import java.io.*;
+
+/**
+ * @author kwang
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	static class Node {
+		int to;
+		int dis;
+		
+		public Node(int to, int dis) {
+			this.to = to;
+			this.dis = dis;
+		}
+	}
+	static List<Node>[] list;
     static boolean[] visited;
-    static int max = 0;
-    static int node;
-    
-    public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
-        
-        int n = scan.nextInt();
-        list = new ArrayList[n + 1]; 
-        for(int i = 1; i < n + 1; i++) {
-            list[i] = new ArrayList<>();
+    static long maxDist = 0L;
+    static int farNode = 0;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		list = new ArrayList[N+1];
+		int[] degree = new int[N+1];
+		for (int i = 1; i <= N; i++) {
+			list[i] = new ArrayList<>();
+		}
+		for (int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			while (true) {
+				int num = Integer.parseInt(st.nextToken());
+				if (num == -1) {
+					break;
+				}
+				int cost = Integer.parseInt(st.nextToken());
+				list[from].add(new Node(num, cost));
+				degree[i]++;
+			}
+		}
+		// 1. 임의의 정점(1번)에서 가장 먼 노드를 찾음
+        visited = new boolean[N + 1];
+        dfs(1, 0L);
+
+        // 2. 찾은 노드에서 다시 가장 먼 거리 탐색
+        visited = new boolean[N + 1];
+        maxDist = 0L;
+        dfs(farNode, 0L);
+
+        System.out.println(maxDist);
+	}
+	private static void dfs(int cur, long sum) {
+		visited[cur] = true;
+
+        if (sum > maxDist) {
+            maxDist = sum;
+            farNode = cur;
         }
-        
-        for(int i = 0; i < n; i++) {
-            int s = scan.nextInt();
-            while(true) {
-                int e = scan.nextInt();
-                if(e == -1) break;
-                int cost = scan.nextInt();
-                list[s].add(new Node(e, cost));
+
+        for (Node next : list[cur]) {
+            if (!visited[next.to]) {
+                dfs(next.to, sum + next.dis);
+                visited[next.to] = true;
             }
         }
-        
-        //임의의 노드(1)에서 부터 가장 먼 노드를 찾는다. 이때 찾은 노드를 node에 저장한다.
-        visited = new boolean[n + 1];
-        dfs(1, 0);
-        
-        //node에서 부터 가장 먼 노트까지의 거리를 구한다.
-        visited = new boolean[n + 1];
-        dfs(node, 0);
-        
-        System.out.println(max);
-    }
-    
-    public static void dfs(int x, int len) {
-        if(len > max) {
-            max = len;
-            node = x;
-        }
-        visited[x] = true;
-        
-        for(int i = 0; i < list[x].size(); i++) {
-            Node n = list[x].get(i);
-            if(visited[n.e] == false) {
-                dfs(n.e, n.cost + len);
-                visited[n.e] = true;
-            }
-        }
-        
-    }
-    
-    public static class Node {
-        int e;
-        int cost;
-        
-        public Node(int e, int cost) {
-            this.e = e;
-            this.cost = cost;
-        }
-    }
+	}
+
 }
